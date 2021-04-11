d76e31d1cf52740b7462263654d8109fc94df588
Matheus <math.reis.dev@gmail.com>
2021-04-11 19:53:09 -0300

    advancing in btns

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
+// Method 1
+
+// let question1 = "Qual o seu nome?";
+// let question2 = "Qual a sua idade?";
+// let question3 = "Qual o seu estado civíl?";
+
+// document.getElementById("btnForward").addEventListener("click", function () {
+//   document.getElementById("outputPer").innerText = question1;
+// });
+
+// document.getElementById("btnBack").addEventListener("click", function () {
+//   document.getElementById("outputPer").innerText = question2;
+// });
+
+// Method 2
+
 const btnBack = document.getElementById("btnBack");
 const btnForward = document.getElementById("btnForward");
 
 btnBack.addEventListener("click", goBack);
 btnForward.addEventListener("click", goForward);
 
+const questions = [
+  "Qual o seu nome?",
+  "Qual a sua idade?",
+  "Qual o seu estado civíl?",
+];
+
 function goBack() {
-  console.log("Back");
+  document.getElementById("outputPer").innerText = questions;
 }
 
 function goForward() {
-  console.log("Foward");
+  document.getElementById("outputPer").innerText = questions;
 }
-
-// var question = document.getElementById("outputPer");
-// var btn = document.getElementById("btnBack");
-// let backQuestion = question1;
-// let backBtn = "Anterior";
-// btn.addEventListener("click", function () {
-//   const tmpquestion = question.innerHTML;
-//   const tmpBtn = btn.innerHTML;
-//   question.innerHTML = backQuestion;
-//   btn.innerHTML = backBtn;
-//   backQuestion = tmpquestion;
-//   backBtn = tmpBtn;
-// });
